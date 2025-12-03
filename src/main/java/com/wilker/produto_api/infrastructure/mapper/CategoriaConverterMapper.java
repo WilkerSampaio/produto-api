4be73558e7d83f151a92fbcdacffd69fb32de4ba
Wilker <neoxextreme@gmail.com>
2025-12-02 22:38:53 -0400

    feat: adicionado mapper

---
+++ src/main/java/com/wilker/produto_api/infrastructure/mapper/CategoriaConverterMapper.java
@@ -0,0 +1,36 @@
+package com.wilker.produto_api.infrastructure.mapper;
+
+import com.wilker.produto_api.infrastructure.dto.in.CategoriaRequestDTO;
+import com.wilker.produto_api.infrastructure.dto.out.CategoriaResponseDTO;
+import com.wilker.produto_api.infrastructure.entity.CategoriaEntity;
+import com.wilker.produto_api.infrastructure.entity.ProdutoEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface CategoriaConverterMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "produtos", ignore = true)
+    CategoriaEntity paraCategoriaEntity(CategoriaRequestDTO categoriaRequestDTO);
+
+
+    @Mapping(target = "idsProdutos", source = "produtos")
+    CategoriaResponseDTO paraCategoriaResponse(CategoriaEntity categoriaEntity);
+
+
+    default Set<Long> mapProdutosToId(Set<ProdutoEntity> produtos){
+            if(produtos == null){
+                return Collections.emptySet();
+            }
+            return produtos.stream()
+                    .map(ProdutoEntity::getId)
+                    .collect(Collectors.toSet());
+
+    }
+
+}
---
+++ src/main/java/com/wilker/produto_api/infrastructure/mapper/ProdutoConverterMapper.java
@@ -0,0 +1,16 @@
+package com.wilker.produto_api.infrastructure.mapper;
+
+import com.wilker.produto_api.infrastructure.dto.in.ProdutoRequestDTO;
+import com.wilker.produto_api.infrastructure.dto.out.ProdutoResponseDTO;
+import com.wilker.produto_api.infrastructure.entity.ProdutoEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProdutoConverterMapper {
+
+    @Mapping(target = "id", ignore = true)
+    ProdutoEntity paraProdutoEntity (ProdutoRequestDTO produtoRequestDTO);
+
+    ProdutoResponseDTO paraProdutoResponse(ProdutoEntity produtoEntity);
+}
