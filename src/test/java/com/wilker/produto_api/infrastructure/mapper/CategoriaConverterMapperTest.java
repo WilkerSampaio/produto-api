beb757aae9b78e6dcdede4d87f25cabd41128e88
Wilker <neoxextreme@gmail.com>
2025-12-09 22:33:13 -0400

    fix: ajuste CategoriaConverterMapperTest

--- src/test/java/com/wilker/produto_api/infrastructure/mapper/CategoriaConverterMapperTest.java
+++ src/test/java/com/wilker/produto_api/infrastructure/mapper/CategoriaConverterMapperTest.java
@@ -14,7 +14,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CategoriaConverterMapperTest {
+class CategoriaConverterMapperTest {
 
     private CategoriaConverterMapper categoriaConverterMapper;
 
