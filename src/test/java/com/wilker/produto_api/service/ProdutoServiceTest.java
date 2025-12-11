9d4d89ea559e0aac21ca2c6307b6beeabe6276fb
Wilker <neoxextreme@gmail.com>
2025-12-10 22:27:29 -0400

    fix: removido importações desnecessárias

--- src/test/java/com/wilker/produto_api/service/ProdutoServiceTest.java
+++ src/test/java/com/wilker/produto_api/service/ProdutoServiceTest.java
@@ -22,8 +22,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
